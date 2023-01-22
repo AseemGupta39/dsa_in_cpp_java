97bbfa8abdd62eb2eecf303df59b5c725c4d22aa
Aseem Gupta <aseemguptajaipur@gmail.com>
2023-01-22 21:28:00 +0530

    added swap function using pointers

--- pointer_to_pointer.cpp
+++ pointer_to_pointer.cpp
@@ -2,10 +2,32 @@
 using namespace std;
 
 void pointer_to_pointer();
+void p_swap(int* a,int* b);
+
 int main()
 {
 	pointer_to_pointer();
+
+	int a = 10;
+	int b = 20;
+
+	cout << a << " " << b << endl;
+
+	int* aptr = &a;
+	int* bptr = &b;
+
+	p_swap(aptr,bptr);
+
+	cout << a << " " << b << endl; 
+
 	return 0;
+}
+
+void p_swap(int* a,int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 void pointer_to_pointer()
